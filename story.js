cdc82412b577911e411ba345cf2f01019a121a6c
eyadbelb <eyadbelb@gmail.com>
2021-05-17 15:39:39 +0300

    your message here

---
+++ story.js
@@ -0,0 +1,8 @@
+
+let name = "eyad";
+
+let age = 25;
+
+let hobby = "gaming";
+
+console.log(` Mr./Mrs. ${name} in 33 years , you will be ${ age += 33} years old . by then you will have decades of ${hobby} practice! `)
